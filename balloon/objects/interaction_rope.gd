2ccdd3d348bcc719f3f0600efaf06dc59941a8de
Eric Li <15696082812@163.com>
2025-09-23 00:33:08 +0200

    grab rope to rotate

--- balloon/objects/interaction_rope.gd
+++ balloon/objects/interaction_rope.gd
@@ -1,21 +1,20 @@
 extends InteractionComponent
 class_name InteractionRope
 
+@export var sensitivity: float = 0.01   # how fast rope moves with mouse
+@export var return_speed: float = 2.0   # how fast it goes back to center when released
+
+var current_percentage: float = 0.0  # -1 (left) .. 1 (right)
+
 func _ready() -> void:
 	super._ready()
+	nodes_to_affect.append(get_tree().get_first_node_in_group("balloon"))
 
 func _input(event):
-	if not is_interacting: return
+	if not is_interacting:
+		return
 
 	if event is InputEventMouseMotion:
-		var delta: float = -event.relative.y * 0.001
-		# Simulate resistance to small motions
-		if abs(delta) < 0.01:
-			delta *= 0.25
-		# Smooth velocity blending
-		print(delta)		
-
-func preInteract(_hand: Marker3D, _target: Node = null) -> void:
-	super.preInteract(_hand, _target) ## put it before the mode check otherwise can't be detected as is_interacting when hold_to_switch mode
-	if not is_occupied:  										   
-		is_occupied = true
+		current_percentage += event.relative.x * 0.01
+		current_percentage = clamp(current_percentage, 0.0, 1.0)
+		notify_nodes(current_percentage)
