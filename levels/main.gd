cde52d701632b9cde14b6a5aea679c857ff76680
Eric Li <15696082812@163.com>
2025-09-25 22:31:15 +0200

    ma

---
+++ levels/main.gd
@@ -0,0 +1,31 @@
+extends Node3D
+
+@export var resource_scenes: Array[PackedScene] = []
+@export var resource_spawn_points: Array[Node3D] = []
+
+@export var island_scenes: Array[PackedScene] = []
+@export var island_spawn_points: Array[Node3D] = []
+
+func _ready() -> void:
+	generate_resource()
+	generate_islands()
+
+func generate_resource():
+	if resource_spawn_points.size() <= 0: return
+	randomize()
+	for point in resource_spawn_points:
+		if resource_scenes.size() > 0:
+			var scene_to_spawn = resource_scenes.pick_random()
+			var instance = scene_to_spawn.instantiate()
+			instance.global_transform = point.global_transform
+			add_child(instance)
+
+func generate_islands():
+	if island_spawn_points.size() <= 0: return
+	randomize()
+	for point in island_spawn_points:
+		if island_scenes.size() > 0:
+			var scene_to_spawn = island_scenes.pick_random()
+			var instance = scene_to_spawn.instantiate()
+			instance.global_transform = point.global_transform
+			add_child(instance)
