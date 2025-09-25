3b870d9ab6403b76e51f99d917a3855c02736079
Reverron <sk8reverron@gmail.com>
2025-09-25 22:27:04 +0200

    FMOD AudioManager v1.2
    Fixed listener issues and polished the script

--- sound/audiomanager/audio_manager.gd
+++ sound/audiomanager/audio_manager.gd
@@ -16,10 +16,23 @@ var sfx_bus: FmodBus
 @export var test_name: String
 var test_event: FmodEvent = null
 
+## -- Listener Settings
+@onready var player: PlayerController
+@onready var listener: FmodListener3D
+
+func _ready() -> void:
+	# references
+	player = get_tree().get_first_node_in_group("player")
+	listener = get_node("Essentials/FmodListener3D")
+
+func _process(_delta: float) -> void:
+	listener.global_transform = player.global_transform
+
 func play(event_name: String, source_transform : Transform3D = global_transform):
 	var event: FmodEvent = FmodServer.create_event_instance(event_name)
-	if source_transform == global_transform :
-		event.set_3d_attributes(source_transform)
-	event.start()
-	event.release()
+	if event.is_valid():
+		if source_transform == global_transform : event.set_3d_attributes(source_transform)
+		event.start()
+		event.release()
+	else: print(event_name + " not found, check for spelling mistakes!")
 	pass
