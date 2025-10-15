8e0cc82561ada6de3dc4237205abde1ced7f9f0c
Eric Li <15696082812@163.com>
2025-10-15 16:47:47 +0200

    f

--- levels/NavigationTest/test_main.gd
+++ levels/NavigationTest/test_main.gd
@@ -2,14 +2,21 @@ extends Node3D
 
 @export var controller_scenes : Array[PackedScene]
 var timer := 0.0
+var level_started := false
+
+@export var win_area: PackedScene
+@export var win_spawn_point: Array[Marker3D]
 
 func _ready():
 	start_level()
 
 func _physics_process(delta: float) -> void:
-	timer += delta
+	if level_started:
+		timer += delta
 
 func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept") and not level_started:
+		level_started = true
 	if event.is_action_pressed("next_level"):
 		next_level()
 	elif event.is_action_pressed("previous_level"):
@@ -23,6 +30,7 @@ func start_level():
 	if index < 0 or index >= controller_scenes.size():
 		return
 	spawn_controller(controller_scenes[index])
+	spawn_win_area()
 
 func restart_level(level_index : int):
 	Global.set_current_level(level_index)
@@ -47,3 +55,15 @@ func next_level():
 func spawn_controller(controller_scene: PackedScene):
 	var controller_instance = controller_scene.instantiate()
 	add_child(controller_instance)
+
+func spawn_win_area():
+	var random_index = randi() % win_spawn_point.size()
+	var spawn_transform = win_spawn_point[random_index].global_transform
+	var win_instance = win_area.instantiate()
+	win_instance.global_transform = spawn_transform
+	add_child(win_instance)
+	win_instance.body_entered.connect(_on_win_body_entered)
+
+func _on_win_body_entered(body: Node3D) -> void:
+	if body is PlayerController:
+		next_level()
