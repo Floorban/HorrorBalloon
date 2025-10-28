69dd99c8f4d760fa554936cb74e7d1804b7d70da
Eric Li <15696082812@163.com>
2025-10-28 20:44:54 +0100

    c

--- camera_follow.gd
+++
@@ -1,7 +0,0 @@
-extends Camera3D
-
-@onready var spring_position: Node3D = $"../SpringArm3D/SpringPosition"
-@export var lerp_power := 1.0
-
-func _process(delta: float) -> void:
-	position = lerp(position, spring_position.position, delta * lerp_power)
---
+++ inventory/item/interaction_pickup.gd
@@ -0,0 +1,44 @@
+extends InteractionComponent
+class_name InteractionPickup
+
+@export var item_data : ItemData
+
+func _ready() -> void:
+	Global.game_start.connect(unfreeze_obj)
+	if item_data == null: return
+	object_ref.name = item_data.item_name
+	spawn_mesh_with_col(item_data.mesh_scene)
+
+func preInteract(_hand: Marker3D, _target: Node = null) -> void:
+	super.postInteract()
+	_pickup_interact()
+
+func _pickup_interact():
+	if ItemInventory.add_item(item_data):
+		object_ref.call_deferred("queue_free")
+		notify_nodes(0, true)
+	else:
+		print("inventory is fulll")
+
+func find_mesh(node: Node) -> MeshInstance3D:
+	if node is MeshInstance3D:
+		return node
+	for child in node.get_children():
+		var result = find_mesh(child)
+		if result: return result
+	return null
+
+func spawn_mesh_with_col(scene) -> Node3D:
+	var inst = scene.instantiate()
+	var mesh_instance = find_mesh(inst)
+	if mesh_instance and mesh_instance.mesh:
+		var shape = mesh_instance.mesh.create_convex_shape()
+		var col = CollisionShape3D.new()
+		col.shape = shape
+		object_ref.add_child.call_deferred(inst)
+		object_ref.add_child.call_deferred(col)
+	return inst
+
+func unfreeze_obj():
+	if object_ref is RigidBody3D:
+		object_ref.freeze = false
